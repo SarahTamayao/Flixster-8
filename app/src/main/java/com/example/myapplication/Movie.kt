c45f5f61353f19a0bca1fa699a280162845b8b5e
jashansingh303 <jashansingh303@gmail.com>
2022-03-04 21:50:32 -0500

    Part 2 Update

--- app/src/main/java/com/example/myapplication/Movie.kt
+++ app/src/main/java/com/example/myapplication/Movie.kt
@@ -1,13 +1,20 @@
 package com.example.myapplication
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import org.json.JSONArray
+
+import kotlinx.parcelize.Parcelize
+@Parcelize
 
 data class Movie (
     val movieID: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview: String,
-) {
+) : Parcelable{
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
     companion object{
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
@@ -17,6 +24,7 @@ data class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                          movieJson.getString("title"),
                         movieJson.getString("overview")
